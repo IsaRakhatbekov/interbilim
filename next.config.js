3d96db23e8f5b32399b698c8fe8ff9c86ad99d64
Mikhaylov-M <mikhaylov.m@outlook.com>
2025-03-25 20:11:29 +0600

    config fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ["drive.google.com"], // Разрешаем Google Drive
     unoptimized: true, // Отключаем оптимизацию
